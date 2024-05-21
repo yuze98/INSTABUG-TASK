7077381a156659146c5bac47104b28b1d4f1a90d
Youssef Hatem <yhmourad98@gmail.com>
2024-05-21 18:42:51 +0300

    added async storage handling for no internet connection

--- src/views/HomeScreen.tsx
+++ src/views/HomeScreen.tsx
@@ -1,4 +1,3 @@
-// HomeScreen.js
 import React, {useEffect, useState} from 'react';
 import {
   FlatList,
@@ -7,53 +6,15 @@ import {
   Image,
   Text,
   TouchableOpacity,
+  Alert,
 } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import NetInfo from '@react-native-community/netinfo';
 import MovieService from '../services/MovieService';
 import {Movie} from '../utils/types';
 
-const DATA = [
-  {
-    id: 1,
-    title: 'Inception',
-    description:
-      'A thief who steals corporate secrets through the use of dream-sharing technology.',
-    imageUri:
-      'https://m.media-amazon.com/images/M/MV5BMTkxNWJkZjItZjk3My00NTQ2LWJmYjItN2Y0MDQ2YjYzYzFhXkEyXkFqcGdeQXVyMjMxOTA1NjA@._V1_.jpg',
-  },
-  {
-    id: 2,
-    title: 'The Dark Knight',
-    description:
-      'When the menace known as The Joker emerges, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
-    imageUri:
-      'https://m.media-amazon.com/images/M/MV5BNzA5ZDQ4YjQtZTM3OS00ZDBiLWJiNzItMTk5NzliYjA5ZGM4XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
-  },
-  {
-    id: 3,
-    title: 'Interstellar',
-    description:
-      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-    imageUri:
-      'https://m.media-amazon.com/images/M/MV5BMjIxMjgxNzM5M15BMl5BanBnXkFtZTgwNjUxNzE3MTE@._V1_.jpg',
-  },
-  {
-    id: 4,
-    title: 'The Matrix',
-    description:
-      'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.',
-    imageUri:
-      'https://m.media-amazon.com/images/M/MV5BNzQzOTk3MTEtNmRjMy00ZjNhLTg5ZGMtYjA3ZTVlNTlhNzE4XkEyXkFqcGdeQXVyNjUxMjc1OTM@._V1_.jpg',
-  },
-  {
-    id: 5,
-    title: 'Avatar',
-    description:
-      'A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.',
-    imageUri:
-      'https://m.media-amazon.com/images/M/MV5BMTYwMzAwNDQxN15BMl5BanBnXkFtZTcwNTI5OTY0Mg@@._V1_.jpg',
-  },
-];
+const STORAGE_KEY = 'MOVIE_LIST';
 
 const renderItem = (item: Movie, navigation: any) => {
   return (
@@ -80,26 +41,46 @@ const HomeScreen = () => {
 
   useEffect(() => {
     const fetchMovieList = async () => {
-      MovieService.getMovieListAxios()
-        .then(res => {
-          setMovies(res.movies);
-        })
-        .catch(err => {
-          setError(err);
-          console.error('An error occured during fetching movie list', err);
-        });
+      const state = await NetInfo.fetch();
+
+      if (state.isConnected) {
+        try {
+          const response = await MovieService.getMovieListAxios();
+          setMovies(response.movies);
+          await AsyncStorage.setItem(
+            STORAGE_KEY,
+            JSON.stringify(response.movies),
+          );
+        } catch (err) {
+          setError('An error occurred during fetching movie list');
+          console.error('An error occurred during fetching movie list', err);
+        }
+      } else {
+        Alert.alert('No Internet Connection', 'Displaying stored movies');
+        const storedMovies = await AsyncStorage.getItem(STORAGE_KEY);
+        if (storedMovies) {
+          setMovies(JSON.parse(storedMovies));
+        } else {
+          setError('No movies stored');
+        }
+      }
     };
     fetchMovieList();
   }, []);
+
   return (
     <View style={styles.container}>
       <Text style={[styles.title, styles.header]}>Your Movie List</Text>
-      <FlatList
-        data={movies}
-        renderItem={({item}) => renderItem(item, navigation)}
-        keyExtractor={item => item.id.toString()}
-        numColumns={2}
-      />
+      {error ? (
+        <Text style={styles.error}>{error}</Text>
+      ) : (
+        <FlatList
+          data={movies}
+          renderItem={({item}) => renderItem(item, navigation)}
+          keyExtractor={item => item.id.toString()}
+          numColumns={2}
+        />
+      )}
     </View>
   );
 };
@@ -132,6 +113,11 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     textAlign: 'center',
     color: '#cccccc',
+  },
+  error: {
+    color: 'red',
+    textAlign: 'center',
+    marginTop: 20,
   },
 });
 
